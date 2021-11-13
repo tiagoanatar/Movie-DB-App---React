4a2936c3744d38e9115e07325a7647bc18ca8859
tiagoanatar <35281635+tiagoanatar@users.noreply.github.com>
2021-11-13 10:27:15 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // @ts-ignore
 import { Link } from "react-router-dom";
 import RMDBLogo from "../../images/movie-app-logo.png";
-import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
+import { Wrapper, Content, LogoImg } from "./Header.styles";
 
 const Header: React.FC = () => {
   return (
